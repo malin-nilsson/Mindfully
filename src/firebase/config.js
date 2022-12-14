b33726e7116d6555647b28f13685967097d5245e
Malin Nilsson <malinnilsson87@gmail.com>
2022-12-14 12:59:56 +0100

    Installed firebase, added config

---
+++ src/firebase/config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyADeIUltIddCu4ak2YN9b6KuoXIYLuCfQc',
+  authDomain: 'finalproject-mi.firebaseapp.com',
+  projectId: 'finalproject-mi',
+  storageBucket: 'finalproject-mi.appspot.com',
+  messagingSenderId: '362050257066',
+  appId: '1:362050257066:web:e450e97cbca0fcbcd7d0d3',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
